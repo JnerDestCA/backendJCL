26c758a1b080eaf03f983422df63b2e7c5b08885
JennerDest169 <cabelloalvaradojenner@gmail.com>
2025-12-26 16:30:32 -0500

    first commit

---
+++ src/main/java/com/pryx/backendJCL/BackendJclApplication.java
@@ -0,0 +1,13 @@
+package com.pryx.backendJCL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pryx/backendJCL/BackendJclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pryx.backendJCL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
